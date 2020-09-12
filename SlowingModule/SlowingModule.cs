ebb3cbf143b1282be3a7d0746bfed5bd1ddfcd4f
Sergey Plashenko <sergey.plashenko@sitecore.com>
2020-09-12 16:30:14 +0300

    Slowing module code.

---
+++ SlowingModule/SlowingModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+using System.Web;
+
+namespace SlowingModule
+{
+    public class SlowingModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            var timeToWaitSetting = ConfigurationManager.AppSettings["SlowingModule:TimeToWait"];
+
+            if (!int.TryParse(timeToWaitSetting, out var timeToWait))
+            {
+                timeToWait = 400000;
+            }
+
+            context.BeginRequest += (object sender, EventArgs e) =>
+            {
+                Thread.Sleep(timeToWait);
+            };
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
